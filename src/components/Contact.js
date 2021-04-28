57a18fefaf37cf25cbe35ed000da8e9fdcbea5c5
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-04-28 19:08:41 -0400

    contact component filled up

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,20 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return <Wrapper>
+    <div className="section-center">
+      <h3>Join Our NewsLetter</h3>
+      <div className="content">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, beatae! Laudantium quam quo dicta libero voluptates incidunt sit, nemo provident.
+        </p>
+        <form action="" className="contact-form">
+          <input type="email" className="form-input" placeholder="Enter Email"/>
+        <button type='submit' className='submit-btn'>Subscribe</button>
+        </form>
+      </div>
+    </div>
+  </Wrapper>
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
