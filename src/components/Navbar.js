0bb32b517524345f937ff2cc3214fe77487dba3d
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-04-27 23:17:00 -0400

    nav bar created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,11 +12,24 @@ const Nav = () => {
   return <NavContainer>
     <div className='nav-center'>
       <div className="nav-header">
-      <Link to="/">
-<img src={logo} alt="logo"/>
-      </Link>
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
+        <button type='button' className='nav-toggle'>
+          <FaBars />
+        </button>
 
       </div>
+      <ul className="nav-links">
+        {links.map((link) => {
+          const { id, text, url } = link;
+          return <li key={id}>
+            <Link to={url}>{text}</Link>
+          </li>
+
+        })}
+      </ul>
+      <CartButtons/>
     </div>
 
   </NavContainer>
