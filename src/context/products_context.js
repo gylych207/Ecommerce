7c0038ec9198a9eb3e697f62b2692600c27419c0
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-04-28 16:59:45 -0400

    side bar open dispatch created

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -13,12 +13,21 @@ import {
   GET_SINGLE_PRODUCT_ERROR,
 } from '../actions'
 
-const initialState = {}
+const initialState = {
+  isSidebarOpen:false
+}
 
 const ProductsContext = React.createContext()
 
 export const ProductsProvider = ({ children }) => {
   const [state,dispatch] = useReducer(reducer,initialState)
+
+  const openSidebar = ()=>{
+    dispatch({ type:SIDEBAR_OPEN})
+  }
+ 
+
+
   return (
     <ProductsContext.Provider value='products context'>
       {children}
