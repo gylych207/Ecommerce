09e7574a3c08a2da69d48784e8306b31cd4ca25c
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-04-28 17:08:50 -0400

    side bar btns applied onclick

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,25 +9,25 @@ import CartButtons from './CartButtons'
 import { useUserContext } from '../context/user_context'
 
 const Sidebar = () => {
- 
-  let isTrue = true;
+  const {isSidebarOpen,closeSidebar} = useProductsContext();
+  
   return <SidebarContainer>
 
-    <aside className={`${isTrue ? 'sidebar show-sidebar' : 'sidebar'}`}>
+    <aside className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
       <div className="sidebar-header">
         <img src={logo} alt="logo" className='logo' />
-        <button className='close-btn' type='button'>
+        <button className='close-btn' type='button' onClick={closeSidebar}>
           <FaTimes />
         </button>
       </div>
     <ul className="links">
       {links.map(({id,text,url})=>{
         return <li key={id}>
-          <Link to={url}>{text}</Link>
+          <Link to={url} onClick={closeSidebar}>{text}</Link>
         </li>
       })}
       <li >
-          <Link to='/checkout'>checkout</Link>
+          <Link to='/checkout' onClick={closeSidebar}>checkout</Link>
         </li>
     </ul>
     <CartButtons/>
