ed8a1e800cd399a1e9a26cf21e901ee71d01590a
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-04-28 22:27:16 -0400

    featured products added

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -14,45 +14,45 @@ import {
 } from '../actions'
 
 const initialState = {
-  isSidebarOpen:false,
-  products_loading:false,
-  products_error:false,
-  products:[],
-  featured_products:[]
+  isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: []
 
 }
 
 const ProductsContext = React.createContext()
 
 export const ProductsProvider = ({ children }) => {
-  const [state,dispatch] = useReducer(reducer,initialState)
+  const [state, dispatch] = useReducer(reducer, initialState)
 
-  const openSidebar = ()=>{
-    dispatch({ type:SIDEBAR_OPEN})
+  const openSidebar = () => {
+    dispatch({ type: SIDEBAR_OPEN })
   }
-  const closeSidebar = ()=>{
-    dispatch({ type:SIDEBAR_CLOSE})
-  }
-
-const fetchProducts = async(url)=>{
-  dispatch({type:GET_PRODUCTS_BEGIN});
-  try {
-    const response = await axios.get(url);
-    const products = response.data;
-    dispatch({ type: GET_PRODUCTS_SUCCESS,payload:products})
-  } catch (error) {
-    dispatch({type:GET_PRODUCTS_ERROR})
+  const closeSidebar = () => {
+    dispatch({ type: SIDEBAR_CLOSE })
   }
 
+  const fetchProducts = async (url) => {
+    dispatch({ type: GET_PRODUCTS_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const products = response.data;
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products })
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR })
+    }
 
 
-}
 
-useEffect(()=>{
-fetchProducts(url)
-},[])
+  }
+
+  useEffect(() => {
+    fetchProducts(url)
+  }, [])
   return (
-    <ProductsContext.Provider value={{...state,openSidebar,closeSidebar}}>
+    <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
       {children}
     </ProductsContext.Provider>
   )
