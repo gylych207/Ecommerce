b64624cca60c7487129cf55c7fe70edb2ccaab4c
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-04-28 22:55:19 -0400

    product action types has been added

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -18,7 +18,11 @@ const initialState = {
   products_loading: false,
   products_error: false,
   products: [],
-  featured_products: []
+  featured_products: [],
+  single_product_loading:false,
+  single_product_error:false,
+  single_produc:{}
+
 
 }
 
@@ -43,14 +47,23 @@ export const ProductsProvider = ({ children }) => {
     } catch (error) {
       dispatch({ type: GET_PRODUCTS_ERROR })
     }
-
-
-
   }
 
+  const fetchSingleProduct = async(url)=>{
+    dispatch({ type:GET_SINGLE_PRODUCT_BEGIN});
+    try {
+      const response = await axios.get(url);
+      const singleProduct = response.data;
+      dispatch({ type:GET_SINGLE_PRODUCT_SUCCESS,payload:singleProduct})
+    } catch (error) {
+      dispatch({ type:GET_SINGLE_PRODUCT_ERROR})
+    }
+  }
+  
   useEffect(() => {
     fetchProducts(url)
   }, [])
+
   return (
     <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
       {children}
