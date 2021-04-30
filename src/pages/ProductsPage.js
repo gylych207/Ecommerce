e40b7dce308fd0754d18e09b685b8b8c8d0c4b1d
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-04-29 23:00:22 -0400

    products page revised

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import { useParams } from 'react-router'
 import styled from 'styled-components'
 import { Filters, ProductList, Sort, PageHero } from '../components'
-import { useProductsContext } from '../context/products_context'
 
 const ProductsPage = () => {
-
-  return <h4>products page</h4>
+  return (
+    <main>
+      <PageHero title='products' />
+      <Wrapper className='page'>
+        <div className='section-center products'>
+          <Filters />
+          <div>
+            <Sort />
+            <ProductList />
+          </div>
+        </div>
+      </Wrapper>
+    </main>
+  )
 }
 
 const Wrapper = styled.div`
