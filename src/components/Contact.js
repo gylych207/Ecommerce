3fe59f5993a00f35511104d3bc76a38a0a19ca53
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-04-30 22:06:49 -0400

    contact added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,31 +2,35 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <Wrapper>
-    <div className="section-center">
-      <h3>Join Our NewsLetter</h3>
-      <div className="content">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, beatae! Laudantium quam quo dicta libero voluptates incidunt sit, nemo provident.
-        </p>
-        <form action=""
-          className="contact-form"
-          action="https://formspree.io/f/xgeroore"
-          method="POST"
-        >
-          <input type="email"
-            className="form-input"
-            placeholder="Enter Email"
-            name="_replyto"
-
-          />
-          <button type='submit'
-
-            className='submit-btn'>Subscribe</button>
-        </form>
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className='content'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+            veniam repudiandae vel ab id, fuga praesentium nobis natus ipsam
+            vero?
+          </p>
+          <form
+            className='contact-form'
+            action='your form spree id'
+            method='POST'
+          >
+            <input
+              type='email'
+              className='form-input'
+              placeholder='enter email'
+              name='_replyto'
+            />
+            <button type='submit' className='submit-btn'>
+              subscribe
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
-  </Wrapper>
+    </Wrapper>
+  )
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
@@ -44,7 +48,6 @@ const Wrapper = styled.section`
     display: grid;
     grid-template-columns: 1fr auto;
   }
-
   .form-input,
   .submit-btn {
     font-size: 1rem;
