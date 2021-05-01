6d96244b8cb6395c8d279facc6472a13a98dc84e
Gylychmyrat Mammedov <gylychmyratmammedov@GylychmyratsMBP.amity.org>
2021-04-30 22:07:52 -0400

    featured product component added

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -8,27 +8,32 @@ import Product from './Product'
 
 const FeaturedProducts = () => {
   const {
-    products_loading:loading,
-     products_error:error,
-     featured_products:featured
-    } = useProductsContext();
-  if(loading){
-    return <Loading/>
+    products_loading: loading,
+    products_error: error,
+    featured_products: featured,
+  } = useProductsContext()
+  if (loading) {
+    return <Loading />
   }
-  if(error){
-    return <Error/>
+  if (error) {
+    return <Error />
   }
-  return <Wrapper className='section'>
-    <div className="title">
-      <h2>featured products</h2>
-      <div className="underline"></div>
-    </div>
-    <div className="section-center featured">
-      {featured.slice(0,3).map((product)=>{
-        return <Product key={product.id} {...product}/>
-      })}
-    </div>
-  </Wrapper>
+  return (
+    <Wrapper className='section'>
+      <div className='title'>
+        <h2>featured products</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='section-center featured'>
+        {featured.slice(0, 3).map((product) => {
+          return <Product key={product.id} {...product} />
+        })}
+      </div>
+      <Link to='/products' className='btn'>
+        all products
+      </Link>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
